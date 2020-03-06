eeeeb87fc4675316af36891a314e27d44bf9ec63
larissasilva <larissasilva.268@gmail.com>
2020-03-05 21:36:27 -0300

    Código da primeira aula de pw

---
+++ aula1/src/main/java/br/edu/utfpr/pb/pw25s/aula1/controller/IndexController.java
@@ -0,0 +1,24 @@
+package br.edu.utfpr.pb.pw25s.aula1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller	
+public class IndexController {
+// @controller é anotação para dizer que class recebe requisições http
+//	é necessário passar o caminho onde ele responde
+	
+	@GetMapping
+	@ResponseBody
+	public String index() {
+		return "hello world!";
+	}
+	
+	@GetMapping("teste")
+	@ResponseBody
+	public String teste() {
+		return "agora é o teste";
+	}
+	
+}
